ed9c4e0de53b086081d986923da7bd564f75c184
STELLAMARUH <stellamaruh@gmail.com>
2022-03-11 03:38:46 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,20 +9,15 @@
  * Return: Always 0 (Success)
 
  */
-
-int main(void)
+int main (void)
 
 {
 
-	int n;
-
-
-
-	for (n = 48; n < 58; n++)
+  int n;
+for (n = 48; n < 58; n++)
 
 	{
-
-		putchar(n);
+ putchar(n);
 
 		if (n != 57)
 
@@ -40,3 +35,4 @@ int main(void)
 
 	return (0);
 
+}
